1e509eed731bc057628faeb9d75908596e8f8bf9
Simon Krull <simondotunix@gmail.com>
2020-03-14 17:04:32 +0100

    Update code for outputting social networks

--- index.php
+++ index.php
@@ -46,9 +46,9 @@
 					
 					<nav>
 							<ul>
-								<li><a target="_blank" href="<?php echo $page->custom('twitter') ?>" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-								<li><a target="_blank" href="<?php echo $page->custom('facebook') ?>" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-								<li><a target="_blank" href="<?php echo $page->custom('github') ?>" class="icon brands fa-github"><span class="label">Github</span></a></li>
+								<li><a target="_blank" href="<?php echo $site->twitter() ?>" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+								<li><a target="_blank" href="<?php echo $site->facebook() ?>" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+								<li><a target="_blank" href="<?php echo $site->github() ?>" class="icon brands fa-github"><span class="label">Github</span></a></li>
 							</ul>
 						</nav>
 					</header>
